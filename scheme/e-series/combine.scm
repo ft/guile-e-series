a572e390d21b88e450989f549adc822d0a75db4f
Frank Terbeck <ft@bewatermyfriend.org>
2018-11-19 17:00:30 +0100

    Add customisable filter to combine
    By default this makes ‘combine’ only include results that are in the 1% error
    range.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/e-series/combine.scm
+++ scheme/e-series/combine.scm
@@ -15,10 +15,12 @@
 ;; Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 (define-module (e-series combine)
+  #:use-module (ice-9 optargs)
   #:use-module (srfi srfi-1)
   #:use-module (e-series tables)
   #:use-module (e-series adjacency)
-  #:export (combine))
+  #:export (combine
+            make-error-filter))
 
 (define (candidate-error t v)
   (/ (- v t) t))
@@ -26,36 +28,46 @@
 (define (value-or-first v)
   (if (list? v) (first v) v))
 
-(define (add+ target lst part adj)
+(define (add+ predicate target lst part adj)
   (let loop ((rest (if (list? adj) adj (list adj))))
     (if (null? rest)
         lst
-        (let ((new (+ part (car rest))))
-          (cons `((combination . direct)
-                  (value . ,new)
-                  (parts ,part ,(car rest))
-                  (error . ,(candidate-error target new)))
-                (loop (cdr rest)))))))
+        (let* ((new (+ part (car rest)))
+               (item `((combination . direct)
+                       (value . ,new)
+                       (parts ,part ,(car rest))
+                       (error . ,(candidate-error target new)))))
+          (if (predicate item)
+              (cons item (loop (cdr rest)))
+              (loop (cdr rest)))))))
 
-(define (add-initial target value)
-  `(((combination . single)
-     (value . ,value)
-     (parts ,value)
-     (error . ,(candidate-error target value)))))
+(define (add-initial predicate target value)
+  (let ((item `(((combination . single)
+                 (value . ,value)
+                 (parts ,value)
+                 (error . ,(candidate-error target value))))))
+    (if (predicate (first item))
+        item
+        '())))
 
 (define (error< a b)
   (< (abs (assq-ref a 'error))
      (abs (assq-ref b 'error))))
 
-(define (combine s value)
+(define (make-error-filter limit)
+  (lambda (item)
+    (<= (abs (assq-ref item 'error)) limit)))
+
+(define* (combine s value #:key (predicate (make-error-filter 1/100)))
   (let* ((nearest (adjacency* s value)))
     (if (number? nearest)
-        (add-initial value nearest)
+        (add-initial predicate value nearest)
         (let ((half (value-or-first (adjacency* s (/ value 2)))))
           (let loop ((current (first nearest))
-                     (candidates (add-initial value (second nearest))))
+                     (candidates (add-initial predicate value
+                                              (second nearest))))
             (if (< current half)
                 (sort candidates error<)
                 (loop (down-e-series s current)
-                      (add+ value candidates current
+                      (add+ predicate value candidates current
                             (adjacency* s (- value current))))))))))
